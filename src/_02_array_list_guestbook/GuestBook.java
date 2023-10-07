1268c8d77bdea9685dae860e7fa8db543424c587
GavinJZhu <gzhu100@icloud.com>
2023-10-07 11:31:23 -0700

    building lineOfNames took like way too long

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -7,47 +7,52 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class GuestBook implements ActionListener {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+    // Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+    // When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
+    // that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+    // all the names added to the list. Format the list as follows:
+    // Guest #1: Bob Banders
+    // Guest #2: Sandy Summers
+    // Guest #3: Greg Ganders
+    // Guest #4: Donny Doners
     Frame uiFrame;
     Button button1;
     Button button2;
     Panel panel;
     ArrayList<String> names = new ArrayList();
+
     public static void main(String[] args) {
-      new GuestBook().run();
+        new GuestBook().run();
     }
-    public void run(){
+
+    public void run() {
         //create a frame
         createFrame();
         //create a panel, add to frame
         //create 2 buttons, add both to panel
         //display the frame
         uiFrame.setVisible(true);
-       createPanel();
-       createButtons();
-       uiFrame.pack();
-       button1.addActionListener(this);
-       button2.addActionListener(this);
-       names.add("Guest #1: Bob Banders");
-       names.add("Guest #2: Sandy Summers");
-       names.add("Guest #3: Greg Ganders");
-       names.add("Guest #4: Donny Doners");
+        createPanel();
+        createButtons();
+        uiFrame.pack();
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        names.add("Guest #1: Bob Banders");
+        names.add("Guest #2: Sandy Summers");
+        names.add("Guest #3: Greg Ganders");
+        names.add("Guest #4: Donny Doners");
     }
-    public void createFrame(){
+
+    public void createFrame() {
         uiFrame = new Frame();
     }
-    public void createPanel(){
-         panel = new Panel();
+
+    public void createPanel() {
+        panel = new Panel();
         uiFrame.add(panel);
     }
-    public void createButtons(){
+
+    public void createButtons() {
         button1 = new Button();
         button2 = new Button();
         panel.add(button1);
@@ -59,23 +64,26 @@ public class GuestBook implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource().equals(button1)){
-            String nameAdd = JOptionPane.showInputDialog(null,"Add a name to the guest book!");
-            int size = names.size()+1;
+        if (e.getSource().equals(button1)) {
+            //retrieve/create strings of names
+            //names = arraylist
+            String nameAdd = JOptionPane.showInputDialog(null, "Add a name to the guest book!");
+            int size = names.size() + 1;
             names.add("Guest #" + size + ": " + nameAdd);
-        }
-        else if (e.getSource().equals(button2)) {
-            for(int i=0; i<1; i++){
+        } else if (e.getSource().equals(button2)) {
+            //display all names, vertically
             // Create String.  lineOfNames
-                String lineOfNames;
+            String lineOfNames = "";
             // Create for loop, iterate/loop all items in names
+            for(int ith=0; ith<names.size(); ith++){
+                //gets string from names
+                String temp = names.get(ith);
+                //build lineOfNames by adding a new name
+                lineOfNames = lineOfNames + temp + "\n";
+                //System.out.println(lineOfNames);
 
-                lineOfNames = lineOfName + names(nth) + "\n";
-
-            // JOptionPane......(....., lineofNames)
-
-        }
-            JOptionPane.showMessageDialog(null, names);
+            }
+            JOptionPane.showMessageDialog(null, lineOfNames);
 
         }
     }
